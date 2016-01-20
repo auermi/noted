3d4f1445d441a9dd693980c904fa9ff897934850
Michael Andrew Auer <mauermcx@gmail.com>
2016-01-20 08:56:11 -0500

    Add note class

---
+++ NoteModel.swift
@@ -0,0 +1,9 @@
+//
+//  NoteModel.swift
+//  Noted
+//
+//  Created by Michael Andrew Auer on 1/19/16.
+//  Copyright © 2016 Usonia LLC. All rights reserved.
+//
+
+import Foundation
---
+++ Noted/Note.swift
@@ -0,0 +1,9 @@
+//
+//  Note.swift
+//  Noted
+//
+//  Created by Michael Andrew Auer on 1/19/16.
+//  Copyright © 2016 Usonia LLC. All rights reserved.
+//
+
+import Foundation
