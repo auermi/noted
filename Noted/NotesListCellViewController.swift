39ba1ab439146f1e06ce6a334d3980321a84ff2e
Michael Andrew Auer <mauermcx@gmail.com>
2016-01-23 01:47:34 -0500

    Refactor the way the table view controller works

---
+++ Noted/NotesListCellViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotesListCellViewController.swift
+//  Noted
+//
+//  Created by Michael Andrew Auer on 1/22/16.
+//  Copyright © 2016 Usonia LLC. All rights reserved.
+//
+
+import Foundation
+
+class NotesListCellViewController: NotesListViewController {
+
+}
