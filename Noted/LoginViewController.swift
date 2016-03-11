0f8055b91624aaf5a31cd0b68b97a092b89e4385
Michael Auer <michael@michael-auer.co>
2016-03-10 21:08:04 -0500

    Add sign in with twitter

---
+++ Noted/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  Login.swift
+//  Noted
+//
+//  Created by Michael Andrew Auer on 3/10/16.
+//  Copyright © 2016 Usonia LLC. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                // Get id and create user if none exists from id
+                print(unwrappedSession.userID)
+                // Transition to notes table view screen
+                self.performSegueWithIdentifier("start", sender: nil)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
